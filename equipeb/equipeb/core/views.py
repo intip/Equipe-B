116957898d8d50b8d430920fed77b2ec2fd7cc64
Arthur B <arthurosbx@gmail.com>
2013-03-28 17:11:19 -0300

    Adiciona template

--- equipeb/equipeb/core/views.py
+++ equipeb/equipeb/core/views.py
@@ -1,8 +1,9 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 import datetime
 
 def home(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    return render_to_response('index.html')
