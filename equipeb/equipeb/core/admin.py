b049bad6e1be8cfc0af1d8c20f760469845ed061
Arthur B <arthurosbx@gmail.com>
2013-03-28 19:05:14 -0300

    novo admin

--- equipeb/equipeb/core/admin.py
+++ equipeb/equipeb/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from equipeb.core.models import Evento, Palestra, Pessoa
+from equipeb.core.models import Evento, Palestra, Visitante, Palestrante
 
 admin.site.register(Evento)
 admin.site.register(Palestra)
-admin.site.register(Pessoa)
+admin.site.register(Visitante)
+admin.site.register(Palestrante)
